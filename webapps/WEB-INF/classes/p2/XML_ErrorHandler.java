bde0b39bcccf6204f3824c412c2980b5df66876a
omardl <105445540+omardl@users.noreply.github.com>
2023-03-06 17:59:59 +0100

    Update XML_ErrorHandler.java

--- webapps/WEB-INF/classes/p2/XML_ErrorHandler.java
+++ webapps/WEB-INF/classes/p2/XML_ErrorHandler.java
@@ -12,95 +12,77 @@ public class XML_ErrorHandler extends DefaultHandler {
 	int fatalerror;
 	ArrayList<String> mensajes;
 	
+	
 	public XML_ErrorHandler() { 
 	
-		this.warning = 0;
-		
+		this.warning = 0;	
 		this.error = 0;
-		
 		this.fatalerror = 0;
-	
-		this.mensajes = new ArrayList<String>();
-		
+		this.mensajes = new ArrayList<String>();	
 	}
+	
 
-    public void warning (SAXParseException w) {
+ 	public void warning (SAXParseException w) {
   
-    	this.mensajes.add(w.toString());
+    		this.mensajes.add(w.toString());
 		this.warning = 1;
-		
 	}
 	
-    public void error(SAXParseException e) {
+	
+    	public void error(SAXParseException e) {
    
-    	
 		this.mensajes.add(e.toString());
-    	this.error = 1;
-    	
+   	 	this.error = 1;
 	}
+	
 
-    public void fatalerror(SAXParseException f) {
+    	public void fatalerror(SAXParseException f) {
         	
-    	this.mensajes.add(f.toString());
-    	this.fatalerror = 1;  	
-    	
-    }
+    		this.mensajes.add(f.toString());
+    		this.fatalerror = 1;  	
+   	 }
+	
 
-    public boolean getWarnings() {
+    	public boolean getWarnings() {
     	
-    	if (this.warning == 1) {
-        	
-    		return true;
-    	
-    	} else {
-    	
-    		return false;
-    	
+    		if (this.warning == 1) {
+    			return true;
+    		} else {
+    			return false;
+    		}
     	}
-    	
-    }
+	
     
-    public boolean getErrors() {
+    	public boolean getErrors() {
     	
-    	if (this.error == 1) {
-    	
-    		return true;
-    	
-    	} else {
-    	
-    		return false;
-    	
+    		if (this.error == 1) {
+    			return true;
+    		} else {
+    			return false;	
+    		}
     	}
-    	
-    }
     
-    public boolean getFatalerrors() {
+	
+    	public boolean getFatalerrors() {
     	
-    	if (this.fatalerror == 1) {
-        	
-    		return true;
-    	
-    	} else {
-    	
-    		return false;
-    	
+    		if (this.fatalerror == 1) {       	
+    			return true;    	
+    		} else {	
+    			return false;    	
+    		}   	
+   	 }
+    
+	
+    	public ArrayList<String> getMessages() { 
+    		return this.mensajes;	
     	}
-    	
-    }
     
-    public ArrayList<String> getMessages() {
-    
-    	return this.mensajes;
+	
+    	public void resetErrorHandler() {
     	
-    }
-    
-    public void resetErrorHandler() {
-    	
-    	this.warning = 0;
-    	this.error = 0;
-    	this.fatalerror = 0;
-    	this.mensajes.clear();
-    	
-    }
-    
+    		this.warning = 0;
+    		this.error = 0;
+    		this.fatalerror = 0;
+    		this.mensajes.clear();
+   	}   
 }
